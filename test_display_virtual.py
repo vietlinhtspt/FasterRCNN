ca43429ded8de3fd276699410ea4b6fbb7c48352
LinhNV <vietlinhtspt@gmail.com>
2020-01-08 11:19:32 +0700

    Same bug: Add save img

--- test_display_virtual.py
+++ test_display_virtual.py
@@ -2,16 +2,22 @@ from dataset.display_virtual import DisplayDemo, register_display
 from viz import draw_annotation
 from dataset.tensorpack.utils.viz import interactive_imshow as imshow
 import cv2
+import os
 from config import finalize_configs
 
 if __name__ == '__main__':
     base_dir = './data'
     roibds = DisplayDemo(base_dir, "train").training_roidbs()
     print("#images:", len(roibds))
-    register_display(base_dir)
     finalize_configs(True)
 
+    i = 0
+
     for r in roibds:
+        i = i + 1
+        if i == 100:
+            break
         im = cv2.imread(r["file_name"])
+        im_out = os.path.join('./output', r["file_name"])
         vis = draw_annotation(im, r["boxes"], r["class"])
-        imshow(vis)
+        cv2.imwrite(im_out, vis)
