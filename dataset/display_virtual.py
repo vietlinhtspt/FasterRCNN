cd85d323ffb712e43b08b58dce3d73bc7c18dc4f
vietlinhtspt <vietlinhtspt@gmail.com>
2020-01-13 10:00:03 +0000

    Add eval function

--- dataset/display_virtual.py
+++ dataset/display_virtual.py
@@ -114,6 +114,116 @@ class DisplayDemo(DatasetSplit):
             dict: the evaluation metrics
                 Ex: {'IoU=0.5:0.95': , 'IoU=0.5': , 'IoU=0.75': , 'small': , 'medium': , 'large': }
         """
+        """
+        Source: https://medium.com/@jonathan_hui/map-mean-average-precision-for-object-detection-45c121a31173
+        We will calculate   |pred_box|label| 
+                         => |IOU|TP/FP| 
+                         => |Precision|Recall|AP| 
+                         => mAP in image
+                         => mAP in all data val
+        """
+        results = self.preprocess_eval(results)
+        totalmAP = 0.0
+        for result in results:
+            # IOU
+            ious = []
+            # TP/FP => 1/0
+            tf = []
+            #----------------
+            precision = [0]
+            # Recall array 
+            recall_cat = np.zeros((len(result['cat'])))
+            print("RECALL CAT:", recall_cat)
+            recall = []
+            # AP
+            ap = []
+            #----------------
+            mAP = 0
+            
+            for index, pred_box in enumerate(result['pred_bboxes']):
+                """
+                {'img_id': '800', 
+                'file_name': '800.png', 
+                'pred_bboxes': [[391.9459, 472.2379, 416.1271, 475.0789], 
+                                [392.0215, 472.2295, 416.1203, 475.0655], 
+                                [401.8083, 469.5519, 404.1204, 480.1884], 
+                                [397.8314, 468.994, 399.9718, 479.4305], 
+                                [419.3113, 465.8, 420.6235, 477.5314], 
+                                [415.1623, 470.0681, 417.6579, 477.0067], 
+                                [397.0155, 469.1784, 407.322, 470.5516], 
+                                [419.3125, 465.8476, 420.6241, 477.5078], 
+                                [306.8225, 411.7572, 309.4693, 418.2742], 
+                                [401.8136, 469.5411, 404.1294, 480.1516]], 
+                                'pred_cat': [1, 2, 1, 1, 1, 1, 1, 2, 1, 2], 
+                'bboxes': array([[285.5, 411.5, 311.5, 417.5],
+                                [224.5, 331.5, 281.5, 341.5],
+                                [395.5, 469.5, 421.5, 475.5]], dtype=float32), 
+                'cat': array([1, 1, 1], dtype=int32)}
+                """
+                max_iou = 0
+                tf = []
+                ap = []
+                # Calculate IOU
+                for i in result['cat']:
+                    pred_cat = result['pred_cat'][index]
+                    if i != pred_cat:
+                        continue
+                    true_box = list(result['bboxes'][i - 1])
+                    print(true_box, type(true_box))
+                    print(pred_box), type(pred_box)
+                    iou = self.calculate_iou(list(pred_box), true_box)
+                    if iou > max_iou:
+                        max_iou = iou
+                ious.append(max_iou)
+                # Thresh IOU, update category detect
+                if iou > 0.45:
+                    tf[index] = 1
+                    recall_cat[pred_cat - 1] = 1
+                
+                precision.append(sum(precision) / len(precision))
+                recall.append(sum(recall_cat) / len(recall_cat))
+                if iou < 0.45:
+                    if len(ap) != 0:
+                        ap.append(ap[-1])
+                    else:
+                        ap = [0]
+                else:
+                    ap.append(precision[-1])
+
+            mAP = sum(ap) / len(ap)
+            results[index]['mAP'] = mAP
+
+            totalmAP = totalmAP + mAP
+            print("[Display_virtual.py:163]mAP.45: ", totalmAP/ len(results))
+
+        return {'mAP.45': totalmAP / len(results)}
+        
+    def preprocess_eval(self, results):
+        """
+        Args:
+            results(list[dict]): result [{'image_id': '999', 
+                                        'category_id': 2, 
+                                        'bbox': [317.0448, 361.4742, 329.8792, 362.6593], 
+                                        'score': 0.0609}]
+        Returns:
+            list[dict]: Concate multi element in result to 1 element with image_id
+                Ex: {'img_id': '999', 
+                'file_name': '999.png',
+                'bboxes': []
+                'cat': []
+                'pred_bboxes': [[318.4033, 348.2516, 338.7042, 350.7122], 
+                            [332.0387, 344.1765, 333.4277, 358.5235], 
+                            [332.0427, 344.126, 333.4282, 358.557], 
+                            [317.3901, 356.4565, 337.6749, 358.8187], 
+                            [323.1426, 352.9561, 337.3448, 354.1138], 
+                            [328.0469, 344.8592, 329.3647, 356.6068], 
+                            [321.6028, 344.4802, 335.1993, 346.0248], 
+                            [323.4732, 348.8036, 337.4124, 349.9655], 
+                            [328.0892, 349.8138, 329.3959, 361.3584], 
+                            [319.6513, 352.7626, 332.4023, 354.0083]], 
+                'pred_cat': [2, 2, 1, 2, 2, 2, 2, 1, 1, 1]}
+        """
+        # HARD CODE: Set dir val to ./data/val
         base_dir = './data'
         roibds = DisplayDemo(base_dir, "val").training_roidbs()
         ret = []
@@ -121,13 +231,22 @@ class DisplayDemo(DatasetSplit):
         for i in range(0, len(results)):
             if i < j:
                 continue
+
             current = {'img_id': results[i]['image_id']}
             current['file_name'] = results[i]['image_id'] + '.png'
-            current['pred_bbox'] = [results[i]['bbox']]
+            current['pred_bboxes'] = [results[i]['bbox']]
             current['pred_cat'] = [results[i]['category_id']]
+
+            for roidb in roibds:
+                file_name = roidb['file_name'].split('/')[-1]
+                #print("[Display_virtual.py:207] ", file_name)
+                if file_name == current['file_name']:
+                    current['bboxes'] = roidb['boxes']
+                    current['cat'] = roidb['class']
+
             for j in range(i + 1, len(results)):
                 if results[j]['image_id'] == current['img_id']:
-                    current['pred_bbox'].append(results[j]['bbox'])
+                    current['pred_bboxes'].append(results[j]['bbox'])
                     current['pred_cat'].append(results[j]['category_id'])
                 else:
                     break
@@ -136,7 +255,7 @@ class DisplayDemo(DatasetSplit):
         """
         {'img_id': '999', 
         'file_name': '999.png', 
-        'pred_bbox': [[318.4033, 348.2516, 338.7042, 350.7122], 
+        'pred_bboxes': [[318.4033, 348.2516, 338.7042, 350.7122], 
                     [332.0387, 344.1765, 333.4277, 358.5235], 
                     [332.0427, 344.126, 333.4282, 358.557], 
                     [317.3901, 356.4565, 337.6749, 358.8187], 
@@ -148,14 +267,11 @@ class DisplayDemo(DatasetSplit):
                     [319.6513, 352.7626, 332.4023, 354.0083]], 
         'pred_cat': [2, 2, 1, 2, 2, 2, 2, 1, 1, 1]}
         """
-        print("Ret length: ", len(ret))
+        print("Results length: ", len(ret))
+        print(ret[-1])
+        return ret
 
-
-            
-
-
-
-    def calculate_iou(boxA, boxB):
+    def calculate_iou(self, boxA, boxB):
         # determine the (x, y)-coordinates of the intersection rectangle
         xA = max(boxA[0], boxB[0])
         yA = max(boxA[1], boxB[1])
@@ -207,3 +323,8 @@ if __name__ == "__main__":
     #     imshow(vis)
 
     DisplayDemo(base_dir, "val").inference_roidbs()
+
+    a = list([285.5, 411.5, 311.5, 417.5])
+    b = [396.5705, 472.431, 419.3307, 475.9445]
+    iou = DisplayDemo.calculate_iou(a, b)
+    print(iou)
